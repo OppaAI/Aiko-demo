ea7c72df3fa1c1f8461587eabfad9a556a702eb4
OppaAI <OppaAI@users.noreply.huggingface.co>
2026-06-09 18:51:01 +0000

    Remove type message

--- app.py
+++ app.py
@@ -99,7 +99,6 @@ with gr.Blocks(title="Aiko-chan 🌸", css=AIKO_CSS, fill_height=True) as demo:
                     elem_id="aiko-chatbot",
                     show_label=False,
                     height=520,
-                    type="messages",
                 )
                 with gr.Row(elem_id="aiko-input-row"):
                     msg = gr.Textbox(
